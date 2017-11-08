5c46ea2b70df91b172cda13ee9a13e3c09b4277c
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-11-08 22:20:22 +0800

    Memasukkan Variabel dan Inputan

--- Tugas 02.cpp
+++ Tugas 02.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
+	int x, y, z;
+	char r;
+	
+	cout<<"PROGRAM HITUNG LUAS"<<endl;
+	cout<<"==================="<<endl;
+	cout<<"P	= Persegi Panjang"<<endl;
+	cout<<"S	= Segitiga"<<endl;
+	cout<<"Ketikkan Huruf [P/S]= "; cin>>r;
+	cout<<end1;
+	
 }
 
